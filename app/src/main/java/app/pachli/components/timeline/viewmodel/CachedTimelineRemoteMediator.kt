46067e6bd5126f026484a16da4ab6255a6766f40
Nik Clayton <nik@ngo.org.uk>
2023-09-14 14:24:25 +0200

    fix: Restore the user's home timeline position
    The previous code would always fetch the latest statuses when the app
    restarts, jumping the user to the top of the home timeline. This is
    because state.anchorPosition was null in this case.
    
    Fix this by passing the saved initialKey to CachedTimelineRemoteMediator
    and using it to construct a page of statuses around the requested
    status.
    
    This restores the user's reading position, and ensures that if the
    user is at the top of the list they're reading position is not reset
    to the second item in the list.
    
    Fixes #41, #42

--- app/src/main/java/app/pachli/components/timeline/viewmodel/CachedTimelineRemoteMediator.kt
+++ app/src/main/java/app/pachli/components/timeline/viewmodel/CachedTimelineRemoteMediator.kt
@@ -36,11 +36,16 @@ import app.pachli.entity.Status
 import app.pachli.network.Links
 import app.pachli.network.MastodonApi
 import com.google.gson.Gson
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import okhttp3.Headers
 import retrofit2.HttpException
+import retrofit2.Response
 import java.io.IOException
 
 @OptIn(ExperimentalPagingApi::class)
 class CachedTimelineRemoteMediator(
+    private val initialKey: String?,
     private val api: MastodonApi,
     accountManager: AccountManager,
     private val factory: InvalidatingPagingSourceFactory<Int, TimelineStatusWithAccount>,
@@ -65,9 +70,10 @@ class CachedTimelineRemoteMediator(
         return try {
             val response = when (loadType) {
                 LoadType.REFRESH -> {
-                    val key = state.anchorPosition?.let { state.closestItemToPosition(it) }?.status?.serverId
-                    Log.d(TAG, "Loading from item close to current position: $key")
-                    api.homeTimeline(minId = key, limit = state.config.pageSize)
+                    val closestItem = state.anchorPosition?.let { state.closestItemToPosition(it) }?.status?.serverId
+                    val key = closestItem ?: initialKey
+                    Log.d(TAG, "Loading from item: $key")
+                    getInitialPage(key, state.config.pageSize)
                 }
                 LoadType.APPEND -> {
                     val rke = db.withTransaction {
@@ -167,6 +173,83 @@ class CachedTimelineRemoteMediator(
         } catch (e: HttpException) {
             MediatorResult.Error(e)
         }
+    }
+
+    /**
+     * Fetch the initial page of statuses, using key as the ID of the initial status to fetch.
+     *
+     * - If there is no key, a page of the most recent statuses is returned
+     * - If the status exists a page that contains the status, and the statuses immediately
+     *   before and after it are returned. This provides enough content that the list adapater
+     *   can restore the user's reading position.
+     * - If the status does not exist the page of statuses immediately before is returned (if
+     *   non-empty)
+     * - If there is no page of statuses immediately before then the page immediately after is
+     *   returned (if non-empty)
+     * - Finally, fall back to the most recent statuses
+     */
+    private suspend fun getInitialPage(statusId: String?, pageSize: Int): Response<List<Status>> = coroutineScope {
+        // If the key is null this is straightforward, just return the most recent statuses.
+        statusId ?: return@coroutineScope api.homeTimeline(limit = pageSize)
+
+        // It's important to return *something* from this state. If an empty page is returned
+        // (even with next/prev links) Pager3 assumes there is no more data to load and stops.
+        //
+        // In addition, the Mastodon API does not let you fetch a page that contains a given key.
+        // You can fetch the page immediately before the key, or the page immediately after, but
+        // you can not fetch the page itself.
+
+        // Fetch the requested status, and the pages immediately before (prev) and after (next)
+        val deferredStatus = async { api.status(statusId = statusId) }
+        val deferredNextPage = async {
+            api.homeTimeline(maxId = statusId, limit = pageSize)
+        }
+        val deferredPrevPage = async {
+            api.homeTimeline(minId = statusId, limit = pageSize)
+        }
+
+        deferredStatus.await().getOrNull()?.let { status ->
+            val statuses = buildList {
+                deferredPrevPage.await().body()?.let { this.addAll(it) }
+                this.add(status)
+                deferredNextPage.await().body()?.let { this.addAll(it) }
+            }
+
+            // "statuses" now contains at least one status we can return, and
+            // hopefully a full page.
+
+            // Build correct max_id and min_id links for the response. The "min_id" to use
+            // when fetching the next page is the same as "key". The "max_id" is the ID of
+            // the oldest status in the list.
+            val minId = statuses.first().id
+            val maxId = statuses.last().id
+            val headers = Headers.Builder()
+                .add("link: </?max_id=$maxId>; rel=\"next\", </?min_id=$minId>; rel=\"prev\"")
+                .build()
+
+            return@coroutineScope Response.success(statuses, headers)
+        }
+
+        // The user's last read status was missing. Use the page of statuses chronologically older
+        // than their desired status. This page must *not* be empty (as noted earlier, if it is,
+        // paging stops).
+        deferredNextPage.await().let { response ->
+            if (response.isSuccessful) {
+                if (!response.body().isNullOrEmpty()) return@coroutineScope response
+            }
+        }
+
+        // There were no statuses older than the user's desired status. Return the page
+        // of statuses immediately newer than their desired status. This page must
+        // *not* be empty (as noted earlier, if it is, paging stops).
+        deferredPrevPage.await().let { response ->
+            if (response.isSuccessful) {
+                if (!response.body().isNullOrEmpty()) return@coroutineScope response
+            }
+        }
+
+        // Everything failed -- fallback to fetching the most recent statuses
+        return@coroutineScope api.homeTimeline(limit = pageSize)
     }
 
     /**
