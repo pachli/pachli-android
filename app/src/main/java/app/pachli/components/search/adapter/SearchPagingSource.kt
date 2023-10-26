ca8c1d91817280c484f5bd9830dbce86b45cafb6
Nik Clayton <nik@ngo.org.uk>
2023-10-26 16:41:12 +0200

    chore: Update copyright notice to refer to Pachli for the license (#201)
    Previous copyright notice mentioned that the license should have been
    distributed with Tusky. Correct that to Pachli.
    
    This does not change the copyright assignment, only the instructions
    as to where to find the license.

--- app/src/main/java/app/pachli/components/search/adapter/SearchPagingSource.kt
+++ app/src/main/java/app/pachli/components/search/adapter/SearchPagingSource.kt
@@ -10,8 +10,9 @@
  * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
  * Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with Tusky; if not,
- * see <http://www.gnu.org/licenses>. */
+ * You should have received a copy of the GNU General Public License along with Pachli; if not,
+ * see <http://www.gnu.org/licenses>.
+ */
 
 package app.pachli.components.search.adapter
 
