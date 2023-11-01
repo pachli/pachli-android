e2410c5ce3c0c6e5d77cbb232ce4cb530653c186
sanao1006 <naosak1006@gmail.com>
2023-11-01 23:10:57 +0900

    feat: Creating a custom lint rule to find `androidx.appcompat.widget.Toolbar`
    From issue (#28), use `com.google.android.material.appbar.MaterialToolbar` instead of `androidx.appcompat.widget.Toolbar`, so avoid `androidx. Appcompat.widget.Toolbar` in the code in the future.
    However, there's a high possibility that a writer could inadvertently write this or that a reviewer could miss it.
    
    Therefore, create a custom lint rule that finds `androidx.appcompat.widget.Toolbar` in the code and warns to use `com.google.android.material.appbar.MaterialToolbar` instead
    
    Fixes #28

---
+++ checks/src/main/java/app/pachli/lint/checks/AndroidxToolbarDetector.kt
@@ -0,0 +1,43 @@
+package app.pachli.lint.checks
+
+import com.android.tools.lint.detector.api.Category
+import com.android.tools.lint.detector.api.Detector
+import com.android.tools.lint.detector.api.Implementation
+import com.android.tools.lint.detector.api.Incident
+import com.android.tools.lint.detector.api.Issue
+import com.android.tools.lint.detector.api.Scope
+import com.android.tools.lint.detector.api.Severity
+import com.android.tools.lint.detector.api.XmlContext
+import com.android.tools.lint.detector.api.XmlScanner
+import org.w3c.dom.Element
+
+class AndroidxToolbarDetector : Detector(), XmlScanner {
+    override fun getApplicableElements(): Collection<String> {
+        return listOf(ANDROIDX_TOOLBAR)
+    }
+
+    override fun visitElement(context: XmlContext, element: Element) {
+        val incident = Incident(context, ISSUE)
+            .message( "Use `<com.google.android.material.appbar.MaterialToolbar>` instead of `<androidx.appcompat.widget.Toolbar>`")
+            .at(element)
+        context.report(incident)
+    }
+
+    companion object {
+        private const val ANDROIDX_TOOLBAR = "androidx.appcompat.widget.Toolbar"
+
+        @JvmField
+        val ISSUE: Issue = Issue.create(
+            id = "AndroidxToolbarDetector",
+            briefDescription = "Don't use `<androidx.appcompat.widget.Toolbar>` in this project",
+            explanation = "<androidx.appcompat.widget.Toolbar> found in source code.\n Use <com.google.android.material.appbar.MaterialToolbar> instead for this project.",
+            category = Category.CORRECTNESS,
+            priority = 6,
+            severity = Severity.WARNING,
+            implementation = Implementation(
+                AndroidxToolbarDetector::class.java,
+                Scope.RESOURCE_FILE_SCOPE,
+            ),
+        )
+    }
+}
---
+++ checks/src/main/java/app/pachli/lint/checks/LintRegistry.kt
@@ -0,0 +1,14 @@
+package app.pachli.lint.checks
+
+import com.android.tools.lint.client.api.IssueRegistry
+import com.android.tools.lint.detector.api.CURRENT_API
+import com.android.tools.lint.detector.api.Issue
+
+@Suppress("UnstableApiUsage")
+class LintRegistry : IssueRegistry() {
+    override val issues: List<Issue>
+        get() = listOf(AndroidxToolbarDetector.ISSUE)
+
+    override val api: Int
+        get() = CURRENT_API
+}
