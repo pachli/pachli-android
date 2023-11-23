225fc2505b865d54c6c1ff701c776829589f0601
Nik Clayton <nik@ngo.org.uk>
2023-11-23 15:25:47 +0100

    Remove obsolete comment re kapt

--- build-logic/convention/src/main/kotlin/AndroidHiltConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidHiltConventionPlugin.kt
@@ -25,8 +25,6 @@ class AndroidHiltConventionPlugin : Plugin<Project> {
         with(target) {
             with(pluginManager) {
                 apply("dagger.hilt.android.plugin")
-                // KAPT must go last to avoid build warnings.
-                // See: https://stackoverflow.com/questions/70550883/warning-the-following-options-were-not-recognized-by-any-processor-dagger-f
                 apply("com.google.devtools.ksp")
             }
 
@@ -37,7 +35,6 @@ class AndroidHiltConventionPlugin : Plugin<Project> {
                 "kspTest"(libs.findLibrary("hilt.compiler").get())
 
                 add("testImplementation", libs.findLibrary("hilt.android.testing").get())
-
             }
         }
     }
