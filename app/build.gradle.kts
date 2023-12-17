f2e25d1482d63eaffc02de84856748a20528c924
Nik Clayton <nik@ngo.org.uk>
2023-12-17 07:03:47 +0100

    chore: Prepare release 2.1.1 (versionCode 10)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
 
     defaultConfig {
         applicationId = "app.pachli"
-        versionCode = 9
-        versionName = "2.1.0"
+        versionCode = 10
+        versionName = "2.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         testInstrumentationRunnerArguments["disableAnalytics"] = "true"
