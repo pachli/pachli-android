cfc6939df3575afa0c48d33c6682edda44553250
Nik Clayton <nik@ngo.org.uk>
2024-02-09 16:14:50 +0100

    fix: Add missing import for MoshiConverterFactory

--- app/src/github/kotlin/app/pachli/di/UpdateCheckModule.kt
+++ app/src/github/kotlin/app/pachli/di/UpdateCheckModule.kt
@@ -26,6 +26,7 @@ import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.create
 
 @InstallIn(SingletonComponent::class)
