75c674fc2b9fded0004526857b366804cfba6747
Nik Clayton <nik@ngo.org.uk>
2024-02-19 11:54:02 +0100

    feat: Display uncropped media on account media pages
    Previously media on the "Media" tab was displayed scaled and cropped to
    a square aspect ratio, effectively forcing the user to tap every image
    to see it.
    
    Now, display the images scaled but not cropped, layed out with
    `StaggeredGridLayoutManager`. This shows each image in full (still
    scaled) providing a better experience when scrolling down.
    
    Scrolling up can occasionally introduce gaps in the grid as images are
    re-placed as viewholders are reused. When this happens images animate
    to a better position when scrolling stops.

--- app/src/main/java/app/pachli/components/account/media/AccountMediaGridAdapter.kt
+++ app/src/main/java/app/pachli/components/account/media/AccountMediaGridAdapter.kt
@@ -1,7 +1,6 @@
 package app.pachli.components.account.media
 
 import android.content.Context
-import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -20,8 +19,6 @@ import app.pachli.util.BindingHolder
 import app.pachli.util.getFormattedDescription
 import app.pachli.util.iconResource
 import com.bumptech.glide.Glide
-import com.google.android.material.color.MaterialColors
-import java.util.Random
 
 class AccountMediaGridAdapter(
     private val useBlurhash: Boolean,
@@ -39,81 +36,71 @@ class AccountMediaGridAdapter(
     },
 ) {
 
-    private val baseItemBackgroundColor = MaterialColors.getColor(context, com.google.android.material.R.attr.colorSurface, Color.BLACK)
     private val playableIcon = AppCompatResources.getDrawable(context, R.drawable.ic_play_indicator)
     private val mediaHiddenDrawable = AppCompatResources.getDrawable(context, R.drawable.ic_hide_media_24dp)
 
-    private val itemBgBaseHSV = FloatArray(3)
-    private val random = Random()
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BindingHolder<ItemAccountMediaBinding> {
         val binding = ItemAccountMediaBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        Color.colorToHSV(baseItemBackgroundColor, itemBgBaseHSV)
-        itemBgBaseHSV[2] = itemBgBaseHSV[2] + random.nextFloat() / 3f - 1f / 6f
-        binding.root.setBackgroundColor(Color.HSVToColor(itemBgBaseHSV))
         return BindingHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: BindingHolder<ItemAccountMediaBinding>, position: Int) {
-        val context = holder.binding.root.context
+    override fun onBindViewHolder(holder: BindingHolder<ItemAccountMediaBinding>, position: Int) = with(holder.binding) {
+        val item = getItem(position) ?: return
 
-        getItem(position)?.let { item ->
-            val imageView = holder.binding.accountMediaImageView
-            val overlay = holder.binding.accountMediaImageViewOverlay
+        val context = root.context
 
-            val placeholder = item.attachment.blurhash?.let {
-                if (useBlurhash) decodeBlurHash(context, it) else null
+        val placeholder = item.attachment.blurhash?.let {
+            if (useBlurhash) decodeBlurHash(context, it) else null
+        }
+
+        when {
+            item.sensitive && !item.isRevealed -> {
+                overlay.show()
+                overlay.setImageDrawable(mediaHiddenDrawable)
+
+                Glide.with(preview)
+                    .load(placeholder)
+                    .centerInside()
+                    .into(preview)
+
+                preview.contentDescription = context.getString(R.string.post_media_hidden_title)
             }
 
-            when {
-                item.sensitive && !item.isRevealed -> {
-                    overlay.show()
-                    overlay.setImageDrawable(mediaHiddenDrawable)
+            item.attachment.isPreviewable() -> {
+                if (item.attachment.type.isPlayable()) overlay.setImageDrawable(playableIcon)
+                overlay.visible(item.attachment.type.isPlayable())
 
-                    Glide.with(imageView)
-                        .load(placeholder)
-                        .centerInside()
-                        .into(imageView)
+                Glide.with(preview)
+                    .asBitmap()
+                    .load(item.attachment.previewUrl)
+                    .placeholder(placeholder)
+//                    .centerInside()
+                    .into(preview)
 
-                    imageView.contentDescription = context.getString(R.string.post_media_hidden_title)
-                }
-
-                item.attachment.isPreviewable() -> {
-                    if (item.attachment.type.isPlayable()) overlay.setImageDrawable(playableIcon)
-                    overlay.visible(item.attachment.type.isPlayable())
-
-                    Glide.with(imageView)
-                        .asBitmap()
-                        .load(item.attachment.previewUrl)
-                        .placeholder(placeholder)
-                        .centerInside()
-                        .into(imageView)
-
-                    imageView.contentDescription = item.attachment.getFormattedDescription(context)
-                }
-
-                else -> {
-                    if (item.attachment.type.isPlayable()) overlay.setImageDrawable(playableIcon)
-                    overlay.visible(item.attachment.type.isPlayable())
-
-                    Glide.with(imageView)
-                        .load(item.attachment.iconResource())
-                        .centerInside()
-                        .into(imageView)
-
-                    imageView.contentDescription = item.attachment.getFormattedDescription(context)
-                }
+                preview.contentDescription = item.attachment.getFormattedDescription(context)
             }
 
-            holder.binding.root.setOnClickListener {
-                onAttachmentClickListener(item, imageView)
-            }
+            else -> {
+                if (item.attachment.type.isPlayable()) overlay.setImageDrawable(playableIcon)
+                overlay.visible(item.attachment.type.isPlayable())
 
-            holder.binding.root.setOnLongClickListener { view ->
-                val description = item.attachment.getFormattedDescription(view.context)
-                Toast.makeText(context, description, Toast.LENGTH_LONG).show()
-                true
+                Glide.with(preview)
+                    .load(item.attachment.iconResource())
+//                    .centerInside()
+                    .into(preview)
+
+                preview.contentDescription = item.attachment.getFormattedDescription(context)
             }
+        }
+
+        root.setOnClickListener {
+            onAttachmentClickListener(item, preview)
+        }
+
+        root.setOnLongClickListener { view ->
+            val description = item.attachment.getFormattedDescription(view.context)
+            Toast.makeText(context, description, Toast.LENGTH_LONG).show()
+            true
         }
     }
 }
