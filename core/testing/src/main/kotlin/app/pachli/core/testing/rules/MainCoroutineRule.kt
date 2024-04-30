1a302213d23a82c24f31d0c9a1a65303e6ee0a12
Nik Clayton <nik@ngo.org.uk>
2024-04-30 16:07:16 +0200

    change: Remove unnecessary `constructor` keyword

--- core/testing/src/main/kotlin/app/pachli/core/testing/rules/MainCoroutineRule.kt
+++ core/testing/src/main/kotlin/app/pachli/core/testing/rules/MainCoroutineRule.kt
@@ -27,7 +27,7 @@ import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 
 @OptIn(ExperimentalCoroutinesApi::class)
-class MainCoroutineRule constructor(private val dispatcher: TestDispatcher = UnconfinedTestDispatcher()) : TestWatcher() {
+class MainCoroutineRule(private val dispatcher: TestDispatcher = UnconfinedTestDispatcher()) : TestWatcher() {
     override fun starting(description: Description) {
         super.starting(description)
         Dispatchers.setMain(dispatcher)
