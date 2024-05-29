pachli/pachli-android

commit 22729df1b76fad8acf7ccb831e41faf1d8f15606

fix: Only enable accessible actions on notifications attached to a status
Fixes #669

app/src/main/java/app/pachli/util/ListStatusAccessibilityDelegate.kt
@@ -20,6 +20,7 @@ import app.pachli.core.activity.openLink
 import app.pachli.core.network.model.Status.Companion.MAX_MEDIA_ATTACHMENTS
 import app.pachli.interfaces.StatusActionListener
 import app.pachli.viewdata.IStatusViewData
+import app.pachli.viewdata.NotificationViewData
 import app.pachli.viewdata.StatusViewData
 import kotlin.math.min
 
@@ -49,6 +50,11 @@ class ListStatusAccessibilityDelegate<T : IStatusViewData>(
 
             val pos = recyclerView.getChildAdapterPosition(host)
             val status = statusProvider.getStatus(pos) ?: return
+
+            // Ignore notifications that don't have an associated statusViewData,
+            // otherwise the accessors throw IllegalStateException.
+            // See https://github.com/pachli/pachli-android/issues/669
+            if ((status as? NotificationViewData)?.statusViewData == null) return
 
             if (status.spoilerText.isNotEmpty()) {
                 info.addAction(if (status.isExpanded) collapseCwAction else expandCwAction)

app/src/main/java/app/pachli/viewdata/NotificationViewData.kt
@@ -72,6 +72,8 @@ data class NotificationViewData(
     // as that might be null. It's up to the calling code to only check these properties if
     // `statusViewData` is not null; not doing that is an illegal state, hence the exception.
 
+    // TODO: Don't do this, it's a significant footgun, see
+    // https://github.com/pachli/pachli-android/issues/669
     override val username: String
         get() = statusViewData?.username ?: throw IllegalStateException()
     override val rebloggedAvatar: String?

commit a39764c0445c0248fae3aec5dd43361772bada59

fix: Re-enable accessiblity actions in all timelines
22729df1 broke accessiblity actions in non-notification timelines by
returning too early if the status was not a NotificationViewData.

app/src/main/java/app/pachli/util/ListStatusAccessibilityDelegate.kt
@@ -54,7 +54,9 @@ class ListStatusAccessibilityDelegate<T : IStatusViewData>(
             // Ignore notifications that don't have an associated statusViewData,
             // otherwise the accessors throw IllegalStateException.
             // See https://github.com/pachli/pachli-android/issues/669
-            if ((status as? NotificationViewData)?.statusViewData == null) return
+            if (status as? NotificationViewData != null) {
+                if (status.statusViewData == null) return
+            }
 
             if (status.spoilerText.isNotEmpty()) {
                 info.addAction(if (status.isExpanded) collapseCwAction else expandCwAction)