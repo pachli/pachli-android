84fdf4abb708e7fbc319a6c310defba9cb3310ea
Nik Clayton <nik@ngo.org.uk>
2024-05-30 14:05:42 +0200

    fix: Only include transitionKind in intents for Pachli activities (#716)
    Previous code always included the transitionKind enum as an extra, and
    could cause a crash if the activity launched by the intent was not a
    Pachli activity.
    
    Fixes #700.

--- core/activity/src/main/kotlin/app/pachli/core/activity/extensions/ActivityExtensions.kt
+++ core/activity/src/main/kotlin/app/pachli/core/activity/extensions/ActivityExtensions.kt
@@ -27,8 +27,12 @@ import app.pachli.core.activity.BaseActivity
 import app.pachli.core.activity.BuildConfig
 
 /**
- * Starts the activity in [intent] (which must be a subclass of [BaseActivity])
- * using [transitionKind] as the open/close transition.
+ * Starts the activity in [intent]. Should only be called on subclasses of [BaseActivity].
+ *
+ * If the activity is a Pachli activity then [transitionKind] is included in the intent and
+ * used as the open/close transition.
+ *
+ * See [BaseActivity.onCreate] for the other half of this code.
  */
 fun Activity.startActivityWithTransition(intent: Intent, transitionKind: TransitionKind) {
     if (BuildConfig.DEBUG) {
@@ -60,7 +64,7 @@ fun Activity.startActivityWithDefaultTransition(intent: Intent) = startActivityW
 fun Activity.setCloseTransition(transitionKind: TransitionKind) {
     if (BuildConfig.DEBUG) {
         if (this !is BaseActivity) {
-            throw IllegalStateException("startActivityWithTransition must be used with BaseActivity subclass")
+            throw IllegalStateException("setCloseTransition must be used with BaseActivity subclass")
         }
     }
 
