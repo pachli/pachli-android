93d4f957a83e2ed7288ba3f2846ff5675316adf6
Nik Clayton <nik@ngo.org.uk>
2024-10-20 15:37:54 +0200

    refactor: Use Date type for scheduled post date / times
    Previous code accepted the `scheduledAt` value as a String, and kept it
    as a String (including when serialising as part of a draft). Then it
    was converted to an actual Date for display.
    
    Refactor to keep it as a Date for as long as possible. Moshi decodes
    Dates correctly over the network, and the database is configured to
    serialise Dates as Longs.
    
    This necessitates two migration steps to preserve any existing
    `scheduledAt` values for drafts. The first step adds a new column to
    store the date as a Long and copies over existing data. The second
    step replaces the old column with the new column.

--- core/database/src/main/kotlin/app/pachli/core/database/AppDatabase.kt
+++ core/database/src/main/kotlin/app/pachli/core/database/AppDatabase.kt
@@ -17,11 +17,17 @@
 
 package app.pachli.core.database
 
+import android.annotation.SuppressLint
+import android.content.ContentValues
+import android.database.sqlite.SQLiteDatabase.CONFLICT_ABORT
+import androidx.core.database.getStringOrNull
 import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.DeleteColumn
+import androidx.room.RenameColumn
 import androidx.room.RoomDatabase
 import androidx.room.migration.AutoMigrationSpec
+import androidx.sqlite.db.SupportSQLiteDatabase
 import app.pachli.core.database.dao.AccountDao
 import app.pachli.core.database.dao.ConversationsDao
 import app.pachli.core.database.dao.DraftDao
@@ -40,6 +46,9 @@ import app.pachli.core.database.model.StatusViewDataEntity
 import app.pachli.core.database.model.TimelineAccountEntity
 import app.pachli.core.database.model.TimelineStatusEntity
 import app.pachli.core.database.model.TranslatedStatusEntity
+import java.text.SimpleDateFormat
+import java.util.Locale
+import java.util.TimeZone
 
 @Suppress("ClassName")
 @Database(
@@ -55,13 +64,15 @@ import app.pachli.core.database.model.TranslatedStatusEntity
         TranslatedStatusEntity::class,
         LogEntryEntity::class,
     ],
-    version = 6,
+    version = 8,
     autoMigrations = [
         AutoMigration(from = 1, to = 2, spec = AppDatabase.MIGRATE_1_2::class),
         AutoMigration(from = 2, to = 3),
         AutoMigration(from = 3, to = 4),
         AutoMigration(from = 4, to = 5),
         AutoMigration(from = 5, to = 6),
+        AutoMigration(from = 6, to = 7, spec = AppDatabase.MIGRATE_6_7::class),
+        AutoMigration(from = 7, to = 8, spec = AppDatabase.MIGRATE_7_8::class),
     ],
 )
 abstract class AppDatabase : RoomDatabase() {
@@ -78,4 +89,54 @@ abstract class AppDatabase : RoomDatabase() {
     @DeleteColumn("TimelineStatusEntity", "contentCollapsed")
     @DeleteColumn("TimelineStatusEntity", "contentShowing")
     class MIGRATE_1_2 : AutoMigrationSpec
+
+    /**
+     * Part one of migrating [DraftEntity.scheduledAt] from String to Long.
+     *
+     * Copies existing data from `scheduledAt` into `scheduledAtLong`.
+     */
+    class MIGRATE_6_7 : AutoMigrationSpec {
+        @SuppressLint("ConstantLocale")
+        private val iso8601 = SimpleDateFormat(
+            "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'",
+            Locale.getDefault(),
+        ).apply {
+            timeZone = TimeZone.getTimeZone("UTC")
+        }
+
+        override fun onPostMigrate(db: SupportSQLiteDatabase) {
+            db.beginTransaction()
+
+            val draftCursor = db.query("SELECT id, scheduledAt FROM DraftEntity")
+            with(draftCursor) {
+                while (moveToNext()) {
+                    val scheduledAt = getStringOrNull(1) ?: continue
+
+                    // Parse the string representation to a Date. Ignore errors, they
+                    // shouldn't be possible.
+                    val scheduledDate = runCatching { iso8601.parse(scheduledAt) }.getOrNull()
+                        ?: continue
+
+                    // Dates are stored as Long, see Converters.dateToLong.
+                    val values = ContentValues().apply {
+                        put("scheduledAtLong", scheduledDate.time)
+                    }
+
+                    val draftId = getInt(0)
+                    db.update("DraftEntity", CONFLICT_ABORT, values, "id = ?", arrayOf(draftId))
+                }
+            }
+
+            db.setTransactionSuccessful()
+            db.endTransaction()
+        }
+    }
+
+    /**
+     * Completes the migration started in [MIGRATE_6_7]. SQLite on Android can't
+     * drop/rename columns, so use Room's annotations to generate the code to do this.
+     */
+    @DeleteColumn("DraftEntity", "scheduledAt")
+    @RenameColumn("DraftEntity", "scheduledAtLong", "scheduledAt")
+    class MIGRATE_7_8 : AutoMigrationSpec
 }
