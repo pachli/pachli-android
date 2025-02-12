3e316eb14a9fe08761637eb494a631bbfd232ba8
Nik Clayton <nik@ngo.org.uk>
2025-02-12 21:55:41 +0100

    change: Ensure DB contains no StatusEntity referencing non-existent AccountEntity (#1292)
    A planned change adds an FK constraint between `StatusEntity` and
    `AccountEntity`.
    
    This may cause a crash if the constraint is added and any existing
    `StatusEntity` reference non-existent `AccountEntity`. So delete them
    now.

--- core/database/src/main/kotlin/app/pachli/core/database/di/DatabaseModule.kt
+++ core/database/src/main/kotlin/app/pachli/core/database/di/DatabaseModule.kt
@@ -23,6 +23,7 @@ import androidx.room.withTransaction
 import app.pachli.core.database.AppDatabase
 import app.pachli.core.database.Converters
 import app.pachli.core.database.MIGRATE_12_13
+import app.pachli.core.database.MIGRATE_18_19
 import app.pachli.core.database.MIGRATE_8_9
 import dagger.Module
 import dagger.Provides
@@ -45,6 +46,7 @@ object DatabaseModule {
             .allowMainThreadQueries()
             .addMigrations(MIGRATE_8_9)
             .addMigrations(MIGRATE_12_13)
+            .addMigrations(MIGRATE_18_19)
             .build()
     }
 
