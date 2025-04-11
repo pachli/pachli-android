pachli/pachli-android

commit b08a0ba282189cbae7f510eabce6599cb57ef56d

feat: Support translation throughout the app (#1414)
Previous code could translate statuses in the home timeline and threads,
but nowhere else.

Extend this support to almost all timelines, in particular:

- Notifications
- Local and federated timelines
- Trending
- Bookmarks
- Favourites

Places where translation is not possible are still:

- Conversations; translation isn't permitted on statuses with `direct`
visibility
- Search results; more work has to be done there

To do this the primary adjustments are to `TimelineViewModel` and
`NetworkTimelineViewModel`. Previously `TimelineViewModel` contained
code that more properly belonged in `CachedTimelineViewModel`; that code
has been moved there, and the actions code in
`{Cached,Network}TimelineViewModel` is now more explicit.

This also fixes a long-standing TODO in `TimelineFragment`, which was
updating adapter data directly instead of relying on the repository data
to change and invalidate the repository.

Similar changes were made to the `Notifications{Timeline,ViewModel}`
files.

Fixes #359

app/src/main/java/app/pachli/components/timeline/TimelineFragment.kt
@@ -50,7 +50,6 @@ import app.pachli.components.timeline.viewmodel.FallibleStatusAction
 import app.pachli.components.timeline.viewmodel.InfallibleStatusAction
 import app.pachli.components.timeline.viewmodel.InfallibleUiAction
 import app.pachli.components.timeline.viewmodel.NetworkTimelineViewModel
-import app.pachli.components.timeline.viewmodel.StatusActionSuccess
 import app.pachli.components.timeline.viewmodel.TimelineViewModel
 import app.pachli.components.timeline.viewmodel.UiError
 import app.pachli.components.timeline.viewmodel.UiSuccess
@@ -327,40 +326,6 @@ class TimelineFragment :
         }
 
         uiResult.onSuccess {
-            // Update adapter data when status actions are successful, and re-bind to update
-            // the UI.
-            // TODO: No - this should be handled by the ViewModel updating the data
-            // and invalidating the paging source
-            if (it is StatusActionSuccess) {
-                val indexedViewData = adapter.snapshot()
-                    .withIndex()
-                    .firstOrNull { indexed ->
-                        indexed.value?.id == it.action.statusViewData.id
-                    } ?: return
-
-                val statusViewData = indexedViewData.value ?: return
-
-                val status = when (it) {
-                    is StatusActionSuccess.Bookmark ->
-                        statusViewData.status.copy(bookmarked = it.action.state)
-
-                    is StatusActionSuccess.Favourite ->
-                        statusViewData.status.copy(favourited = it.action.state)
-
-                    is StatusActionSuccess.Reblog ->
-                        statusViewData.status.copy(reblogged = it.action.state)
-
-                    is StatusActionSuccess.VoteInPoll ->
-                        statusViewData.status.copy(
-                            poll = it.action.poll.votedCopy(it.action.choices),
-                        )
-
-                    is StatusActionSuccess.Translate -> statusViewData.status
-                }
-                (indexedViewData.value as StatusViewData).status = status
-                adapter.notifyItemChanged(indexedViewData.index)
-            }
-
             // Refresh adapter on mutes and blocks
             when (it) {
                 is UiSuccess.Block, is UiSuccess.Mute, is UiSuccess.MuteConversation,
@@ -607,11 +572,11 @@ class TimelineFragment :
     }
 
     override fun onExpandedChange(viewData: StatusViewData, expanded: Boolean) {
-        viewModel.changeExpanded(expanded, viewData)
+        viewModel.onChangeExpanded(expanded, viewData)
     }
 
     override fun onContentHiddenChange(viewData: StatusViewData, isShowingContent: Boolean) {
-        viewModel.changeContentShowing(isShowingContent, viewData)
+        viewModel.onChangeContentShowing(isShowingContent, viewData)
     }
 
     override fun onShowReblogs(statusId: String) {
@@ -625,11 +590,10 @@ class TimelineFragment :
     }
 
     override fun onContentCollapsedChange(viewData: StatusViewData, isCollapsed: Boolean) {
-        viewModel.changeContentCollapsed(isCollapsed, viewData)
+        viewModel.onContentCollapsed(isCollapsed, viewData)
     }
 
-    // Can only translate the home timeline at the moment
-    override fun canTranslate() = timeline == Timeline.Home
+    override fun canTranslate() = true
 
     override fun onTranslate(statusViewData: StatusViewData) {
         viewModel.accept(FallibleStatusAction.Translate(statusViewData))

app/src/main/java/app/pachli/components/conversation/ConversationsFragment.kt
@@ -349,6 +349,9 @@ class ConversationsFragment :
         adapter.refresh()
     }
 
+    // Can't translate conversations because of Mastodon privacy settings.
+    override fun canTranslate() = false
+
     override fun onReblog(viewData: ConversationViewData, reblog: Boolean) {
         // its impossible to reblog private messages
     }

app/src/main/java/app/pachli/components/notifications/NotificationsFragment.kt
@@ -480,23 +480,37 @@ class NotificationsFragment :
     }
 
     override fun onReblog(viewData: NotificationViewData, reblog: Boolean) {
-        viewModel.accept(StatusAction.Reblog(reblog, viewData.statusViewData!!))
+        viewModel.accept(FallibleStatusAction.Reblog(reblog, viewData.statusViewData!!))
     }
 
     override fun onFavourite(viewData: NotificationViewData, favourite: Boolean) {
-        viewModel.accept(StatusAction.Favourite(favourite, viewData.statusViewData!!))
+        viewModel.accept(FallibleStatusAction.Favourite(favourite, viewData.statusViewData!!))
     }
 
     override fun onBookmark(viewData: NotificationViewData, bookmark: Boolean) {
-        viewModel.accept(StatusAction.Bookmark(bookmark, viewData.statusViewData!!))
+        viewModel.accept(FallibleStatusAction.Bookmark(bookmark, viewData.statusViewData!!))
     }
 
     override fun onVoteInPoll(viewData: NotificationViewData, poll: Poll, choices: List<Int>) {
-        viewModel.accept(StatusAction.VoteInPoll(poll, choices, viewData.statusViewData!!))
+        viewModel.accept(FallibleStatusAction.VoteInPoll(poll, choices, viewData.statusViewData!!))
     }
 
     override fun onMore(view: View, viewData: NotificationViewData) {
         super.more(view, viewData)
+    }
+
+    override fun canTranslate() = true
+
+    override fun onTranslate(statusViewData: NotificationViewData) {
+        statusViewData.statusViewData?.let {
+            viewModel.accept(FallibleStatusAction.Translate(it))
+        }
+    }
+
+    override fun onTranslateUndo(statusViewData: NotificationViewData) {
+        statusViewData.statusViewData?.let {
+            viewModel.accept(InfallibleStatusAction.TranslateUndo(it))
+        }
     }
 
     override fun onViewMedia(viewData: NotificationViewData, attachmentIndex: Int, view: View?) {

app/src/main/java/app/pachli/components/notifications/NotificationsViewModel.kt
@@ -243,45 +243,59 @@ sealed interface NotificationActionSuccess : UiSuccess {
     }
 }
 
+sealed interface StatusAction
+
+sealed interface InfallibleStatusAction : InfallibleUiAction, StatusAction {
+    val statusViewData: StatusViewData
+
+    data class TranslateUndo(override val statusViewData: StatusViewData) : InfallibleStatusAction
+}
+
 /** Actions the user can trigger on an individual status */
-sealed interface StatusAction : FallibleUiAction {
+sealed interface FallibleStatusAction : FallibleUiAction, StatusAction {
     val statusViewData: StatusViewData
 
     /** Set the bookmark state for a status */
-    data class Bookmark(val state: Boolean, override val statusViewData: StatusViewData) : StatusAction
+    data class Bookmark(val state: Boolean, override val statusViewData: StatusViewData) : FallibleStatusAction
 
     /** Set the favourite state for a status */
-    data class Favourite(val state: Boolean, override val statusViewData: StatusViewData) : StatusAction
+    data class Favourite(val state: Boolean, override val statusViewData: StatusViewData) : FallibleStatusAction
 
     /** Set the reblog state for a status */
-    data class Reblog(val state: Boolean, override val statusViewData: StatusViewData) : StatusAction
+    data class Reblog(val state: Boolean, override val statusViewData: StatusViewData) : FallibleStatusAction
 
     /** Vote in a poll */
     data class VoteInPoll(
         val poll: Poll,
         val choices: List<Int>,
         override val statusViewData: StatusViewData,
-    ) : StatusAction
+    ) : FallibleStatusAction
+
+    /** Translate a status */
+    data class Translate(override val statusViewData: StatusViewData) : FallibleStatusAction
 }
 
 /** Changes to a status' visible state after API calls */
 sealed interface StatusActionSuccess : UiSuccess {
-    val action: StatusAction
+    val action: FallibleStatusAction
 
-    data class Bookmark(override val action: StatusAction.Bookmark) : StatusActionSuccess
+    data class Bookmark(override val action: FallibleStatusAction.Bookmark) : StatusActionSuccess
 
-    data class Favourite(override val action: StatusAction.Favourite) : StatusActionSuccess
+    data class Favourite(override val action: FallibleStatusAction.Favourite) : StatusActionSuccess
 
-    data class Reblog(override val action: StatusAction.Reblog) : StatusActionSuccess
+    data class Reblog(override val action: FallibleStatusAction.Reblog) : StatusActionSuccess
 
-    data class VoteInPoll(override val action: StatusAction.VoteInPoll) : StatusActionSuccess
+    data class VoteInPoll(override val action: FallibleStatusAction.VoteInPoll) : StatusActionSuccess
+
+    data class Translate(override val action: FallibleStatusAction.Translate) : StatusActionSuccess
 
     companion object {
-        fun from(action: StatusAction) = when (action) {
-            is StatusAction.Bookmark -> Bookmark(action)
-            is StatusAction.Favourite -> Favourite(action)
-            is StatusAction.Reblog -> Reblog(action)
-            is StatusAction.VoteInPoll -> VoteInPoll(action)
+        fun from(action: FallibleStatusAction) = when (action) {
+            is FallibleStatusAction.Bookmark -> Bookmark(action)
+            is FallibleStatusAction.Favourite -> Favourite(action)
+            is FallibleStatusAction.Reblog -> Reblog(action)
+            is FallibleStatusAction.VoteInPoll -> VoteInPoll(action)
+            is FallibleStatusAction.Translate -> Translate(action)
         }
     }
 }
@@ -306,26 +320,32 @@ sealed interface UiError {
 
     data class Bookmark(
         override val error: PachliError,
-        override val action: StatusAction.Bookmark,
+        override val action: FallibleStatusAction.Bookmark,
         override val message: Int = R.string.ui_error_bookmark_fmt,
     ) : UiError
 
     data class Favourite(
         override val error: PachliError,
-        override val action: StatusAction.Favourite,
+        override val action: FallibleStatusAction.Favourite,
         override val message: Int = R.string.ui_error_favourite_fmt,
     ) : UiError
 
     data class Reblog(
         override val error: PachliError,
-        override val action: StatusAction.Reblog,
+        override val action: FallibleStatusAction.Reblog,
         override val message: Int = R.string.ui_error_reblog_fmt,
     ) : UiError
 
     data class VoteInPoll(
         override val error: PachliError,
-        override val action: StatusAction.VoteInPoll,
+        override val action: FallibleStatusAction.VoteInPoll,
         override val message: Int = R.string.ui_error_vote_fmt,
+    ) : UiError
+
+    data class TranslateStatus(
+        override val error: PachliError,
+        override val action: FallibleStatusAction.Translate,
+        override val message: Int = R.string.ui_error_translate_status_fmt,
     ) : UiError
 
     data class AcceptFollowRequest(
@@ -348,10 +368,11 @@ sealed interface UiError {
 
     companion object {
         fun make(error: PachliError, action: FallibleUiAction) = when (action) {
-            is StatusAction.Bookmark -> Bookmark(error, action)
-            is StatusAction.Favourite -> Favourite(error, action)
-            is StatusAction.Reblog -> Reblog(error, action)
-            is StatusAction.VoteInPoll -> VoteInPoll(error, action)
+            is FallibleStatusAction.Bookmark -> Bookmark(error, action)
+            is FallibleStatusAction.Favourite -> Favourite(error, action)
+            is FallibleStatusAction.Reblog -> Reblog(error, action)
+            is FallibleStatusAction.VoteInPoll -> VoteInPoll(error, action)
+            is FallibleStatusAction.Translate -> TranslateStatus(error, action)
             is NotificationAction.AcceptFollowRequest -> AcceptFollowRequest(error, action)
             is NotificationAction.RejectFollowRequest -> RejectFollowRequest(error, action)
             FallibleUiAction.ClearNotifications -> ClearNotifications(error)
@@ -476,37 +497,39 @@ class NotificationsViewModel @AssistedInject constructor(
 
         // Handle StatusAction.*
         viewModelScope.launch {
-            uiAction.filterIsInstance<StatusAction>()
+            uiAction.filterIsInstance<FallibleStatusAction>()
                 .throttleFirst() // avoid double-taps
                 .collect { action ->
                     val result = when (action) {
-                        is StatusAction.Bookmark -> statusRepository.bookmark(
+                        is FallibleStatusAction.Bookmark -> statusRepository.bookmark(
                             pachliAccountId,
                             action.statusViewData.actionableId,
                             action.state,
                         )
 
-                        is StatusAction.Favourite -> statusRepository.favourite(
+                        is FallibleStatusAction.Favourite -> statusRepository.favourite(
                             pachliAccountId,
                             action.statusViewData.actionableId,
                             action.state,
                         )
 
-                        is StatusAction.Reblog -> statusRepository.reblog(
+                        is FallibleStatusAction.Reblog -> statusRepository.reblog(
                             pachliAccountId,
                             action.statusViewData.actionableId,
                             action.state,
                         )
 
-                        is StatusAction.VoteInPoll -> statusRepository.voteInPoll(
+                        is FallibleStatusAction.VoteInPoll -> statusRepository.voteInPoll(
                             pachliAccountId,
                             action.statusViewData.actionableId,
                             action.poll.id,
                             action.choices,
                         )
+
+                        is FallibleStatusAction.Translate -> timelineCases.translate(action.statusViewData)
                     }.mapEither(
                         { StatusActionSuccess.from(action) },
-                        { UiError.make(it.error, action) },
+                        { UiError.make(it, action) },
                     )
                     _uiResult.send(result)
                 }
@@ -535,6 +558,13 @@ class NotificationsViewModel @AssistedInject constructor(
                     is MuteEvent -> _uiResult.send(Ok(UiSuccess.Mute))
                     is MuteConversationEvent -> _uiResult.send(Ok(UiSuccess.MuteConversation))
                 }
+            }
+        }
+
+        // Undo status translations
+        viewModelScope.launch {
+            uiAction.filterIsInstance<InfallibleStatusAction.TranslateUndo>().collectLatest {
+                timelineCases.translateUndo(it.statusViewData)
             }
         }
 

app/src/main/java/app/pachli/components/timeline/CachedTimelineRepository.kt
@@ -26,7 +26,6 @@ import app.pachli.components.timeline.TimelineRepository.Companion.PAGE_SIZE
 import app.pachli.components.timeline.viewmodel.CachedTimelineRemoteMediator
 import app.pachli.components.timeline.viewmodel.CachedTimelineRemoteMediator.Companion.RKE_TIMELINE_ID
 import app.pachli.core.common.di.ApplicationScope
-import app.pachli.core.data.repository.StatusRepository
 import app.pachli.core.database.dao.RemoteKeyDao
 import app.pachli.core.database.dao.StatusDao
 import app.pachli.core.database.dao.TimelineDao
@@ -64,7 +63,6 @@ class CachedTimelineRepository @Inject constructor(
     private val remoteKeyDao: RemoteKeyDao,
     private val translatedStatusDao: TranslatedStatusDao,
     private val statusDao: StatusDao,
-    private val statusRepository: StatusRepository,
     @ApplicationScope private val externalScope: CoroutineScope,
 ) : TimelineRepository<TimelineStatusWithAccount> {
     private var factory: InvalidatingPagingSourceFactory<Int, TimelineStatusWithAccount>? = null

app/src/main/java/app/pachli/components/timeline/viewmodel/CachedTimelineViewModel.kt
@@ -36,7 +36,6 @@ import app.pachli.core.eventhub.FavoriteEvent
 import app.pachli.core.eventhub.PinEvent
 import app.pachli.core.eventhub.ReblogEvent
 import app.pachli.core.model.FilterAction
-import app.pachli.core.network.model.Poll
 import app.pachli.core.preferences.SharedPreferencesRepository
 import app.pachli.usecase.TimelineCases
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -104,10 +103,6 @@ class CachedTimelineViewModel @Inject constructor(
             }
     }
 
-    override fun updatePoll(newPoll: Poll, status: StatusViewData) {
-        // handled by CacheUpdater
-    }
-
     override fun removeAllByAccountId(pachliAccountId: Long, accountId: String) {
         viewModelScope.launch {
             repository.removeAllByAccountId(pachliAccountId, accountId)
@@ -145,6 +140,56 @@ class CachedTimelineViewModel @Inject constructor(
     override fun handlePinEvent(pinEvent: PinEvent) {
         // handled by CacheUpdater
     }
+
+    override fun onChangeExpanded(isExpanded: Boolean, statusViewData: StatusViewData) {
+        viewModelScope.launch {
+            statusRepository.setExpanded(statusViewData.pachliAccountId, statusViewData.id, isExpanded)
+            repository.invalidate(statusViewData.pachliAccountId)
+        }
+    }
+
+    override fun onChangeContentShowing(isShowing: Boolean, statusViewData: StatusViewData) {
+        viewModelScope.launch {
+            statusRepository.setContentShowing(statusViewData.pachliAccountId, statusViewData.id, isShowing)
+            repository.invalidate(statusViewData.pachliAccountId)
+        }
+    }
+
+    override fun onContentCollapsed(isCollapsed: Boolean, statusViewData: StatusViewData) {
+        viewModelScope.launch {
+            statusRepository.setContentCollapsed(statusViewData.pachliAccountId, statusViewData.id, isCollapsed)
+            repository.invalidate(statusViewData.pachliAccountId)
+        }
+    }
+
+    override suspend fun onBookmark(action: FallibleStatusAction.Bookmark) = statusRepository.bookmark(
+        action.statusViewData.pachliAccountId,
+        action.statusViewData.actionableId,
+        action.state,
+    )
+
+    override suspend fun onFavourite(action: FallibleStatusAction.Favourite) = statusRepository.favourite(
+        action.statusViewData.pachliAccountId,
+        action.statusViewData.actionableId,
+        action.state,
+    )
+
+    override suspend fun onReblog(action: FallibleStatusAction.Reblog) = statusRepository.reblog(
+        action.statusViewData.pachliAccountId,
+        action.statusViewData.actionableId,
+        action.state,
+    )
+
+    override suspend fun onVoteInPoll(action: FallibleStatusAction.VoteInPoll) = statusRepository.voteInPoll(
+        action.statusViewData.pachliAccountId,
+        action.statusViewData.actionableId,
+        action.poll.id,
+        action.choices,
+    )
+
+    override suspend fun onTranslate(action: FallibleStatusAction.Translate) = timelineCases.translate(action.statusViewData)
+
+    override suspend fun onUndoTranslate(action: InfallibleStatusAction.TranslateUndo) = timelineCases.translateUndo(action.statusViewData)
 
     override suspend fun invalidate(pachliAccountId: Long) {
         repository.invalidate(pachliAccountId)

commit 6d1a31ea5ffb2de03caf193173b778c0d42efa87

fix: Set initial reading position for non-cached timelines to the top
This was earlier behaviour, but by accident. b08a0ba28218 broke this when
database lookups were added in `NetworkTimelineViewModel.getStatuses()`.

The data didn't need to be used -- simply making the database lookup was
enough to lose the race. This could also be dependent on network timing and
the speed the remote server returned results, so never showed up in local
tests.

This changed the timing for when results were returned, resulting in
`state.anchorPosition` being much lower down the list (i.e,. instead of
being e.g., `2` it might be `39`.

app/src/main/java/app/pachli/components/timeline/TimelineFragment.kt
@@ -231,8 +231,9 @@ class TimelineFragment :
         viewLifecycleOwner.lifecycleScope.launch {
             viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
                 launch {
-                    // Wait for the very first page load, then scroll recyclerview
-                    // to the refresh key.
+                    // If this is a cached timeline there's a refresh key. Wait for the very
+                    // first page load, then scroll recyclerview to the refresh key so any
+                    // remaining page loads keep it in focus.
                     (viewModel as? CachedTimelineViewModel)?.let { vm ->
                         vm.initialRefreshKey.combine(adapter.onPagesUpdatedFlow) { key, _ -> key }
                             .take(1)
@@ -240,10 +241,24 @@ class TimelineFragment :
                             .collect { key ->
                                 val snapshot = adapter.snapshot()
                                 val index = snapshot.items.indexOfFirst { it.id == key }
-                                binding.recyclerView.scrollToPosition(
-                                    snapshot.placeholdersBefore + index,
-                                )
+                                val position = snapshot.placeholdersBefore + index
+                                binding.recyclerView.post {
+                                    getView() ?: return@post
+                                    binding.recyclerView.scrollToPosition(position)
+                                }
                             }
+                    }
+
+                    // If this is a non-cached timeline the user should be placed at the
+                    // top of the timeline. Wait for the prepend operations to complete
+                    // then do that.
+                    (viewModel as? NetworkTimelineViewModel)?.let { vm ->
+                        adapter.postPrepend {
+                            binding.recyclerView.post {
+                                getView() ?: return@post
+                                binding.recyclerView.scrollToPosition(0)
+                            }
+                        }
                     }
                 }
 

app/src/main/java/app/pachli/components/timeline/viewmodel/NetworkTimelinePagingSource.kt
@@ -56,9 +56,9 @@ class NetworkTimelinePagingSource @Inject constructor(
         }
 
         if (page == null) {
-            Timber.d("  Returning empty page")
+            Timber.d("  Returning empty page for %s", params.javaClass.simpleName)
         } else {
-            Timber.d("  Returning full page:")
+            Timber.d("  Returning full page for %s", params.javaClass.simpleName)
             Timber.d("     %s", page)
         }
 
@@ -66,7 +66,7 @@ class NetworkTimelinePagingSource @Inject constructor(
         // is a lot of spurious animation, especially during the initial load, as multiple pages
         // are loaded and the paging source is repeatedly invalidated.
         if (invalid) {
-            Timber.d("Invalidated, returning LoadResult.Invalid")
+            Timber.d("Invalidated, returning LoadResult.Invalid for %s", params.javaClass.simpleName)
             return INVALID
         }
 