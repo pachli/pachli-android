pachli/pachli-android

commit 9988932f187c8f26879675399745a5d9aa8f6ab6

refactor: Filter statuses early (#1774)
Previous code constructed `StatusViewData` objects for each status in
the timeline, then applied any `Hide` filters.

Now, determine the filter action for each status first, filter out any
that should be hidden, then create the `StatusViewData`.

app/src/main/java/app/pachli/components/timeline/viewmodel/TimelineViewModel.kt
@@ -34,6 +34,7 @@ import app.pachli.core.data.repository.AccountManager
 import app.pachli.core.data.repository.OfflineFirstStatusRepository
 import app.pachli.core.data.repository.StatusActionError
 import app.pachli.core.data.repository.StatusDisplayOptionsRepository
+import app.pachli.core.database.model.TimelineStatusWithAccount
 import app.pachli.core.eventhub.BlockEvent
 import app.pachli.core.eventhub.BookmarkEvent
 import app.pachli.core.eventhub.DomainMuteEvent
@@ -589,17 +590,27 @@ abstract class TimelineViewModel<T : Any, R : TimelineRepository<T>>(
     /** Triggered when currently displayed data must be reloaded. */
     protected abstract suspend fun invalidate(pachliAccountId: Long)
 
-    protected fun shouldFilterStatus(status: Status): FilterAction {
-        return if (
-            (status.inReplyToId != null && filterRemoveReplies) ||
-            (status.reblog != null && filterRemoveReblogs) ||
-            // To determine if the boost is boosting your own toot
-            ((status.account.id == status.reblog?.account?.id) && filterRemoveSelfReblogs)
-        ) {
-            FilterAction.HIDE
-        } else {
-            contentFilterModel?.filterActionFor(status) ?: FilterAction.NONE
+    /**
+     * @return The correct [FilterAction] for [timelineStatus] given the user's
+     * preferences and any filters attached to the status.
+     */
+    protected fun shouldFilterStatus(timelineStatus: TimelineStatusWithAccount): FilterAction {
+        // Local user preferences first
+
+        // Remove self-boosts.
+        if (timelineStatus.account.serverId == timelineStatus.reblogAccount?.serverId && filterRemoveSelfReblogs) {
+            return FilterAction.HIDE
         }
+
+        val status = timelineStatus.status
+        // Remove replies
+        if (status.inReplyToId != null && filterRemoveReplies) return FilterAction.HIDE
+
+        // Remove boosts
+        if (status.reblogged && filterRemoveReblogs) return FilterAction.HIDE
+
+        // Apply content filters.
+        return contentFilterModel?.filterActionFor(status) ?: FilterAction.NONE
     }
 
     // TODO: Update this so that the list of UIPrefs is correct

app/src/main/java/app/pachli/components/conversation/ConversationsViewModel.kt
@@ -67,32 +67,31 @@ class ConversationsViewModel @AssistedInject constructor(
     private val uiAction = MutableSharedFlow<UiAction>()
     val accept: (UiAction) -> Unit = { action -> viewModelScope.launch { uiAction.emit(action) } }
 
-    val conversationFlow = accountFlow
-        .flatMapLatest { pachliAccount ->
-            repository.conversations(pachliAccount.id)
-                .map { pagingData ->
-                    pagingData
-                        .map { conversation ->
-                            val accountFilterDecision = if (conversation.isConversationStarter) {
-                                conversation.viewData?.accountFilterDecision
-                                    ?: filterConversationByAccount(pachliAccount, conversation)
-                            } else {
-                                null
-                            }
-
-                            ConversationViewData.make(
-                                pachliAccount,
-                                conversation,
-                                defaultIsExpanded = pachliAccount.entity.alwaysOpenSpoiler,
-                                defaultIsShowingContent = (pachliAccount.entity.alwaysShowSensitiveMedia || !conversation.lastStatus.status.sensitive),
-                                contentFilterAction = FilterAction.NONE,
-                                accountFilterDecision = accountFilterDecision,
-                            )
-                        }
-                        .filter { it.accountFilterDecision !is AccountFilterDecision.Hide }
+    val conversationFlow = accountFlow.flatMapLatest { pachliAccount ->
+        repository.conversations(pachliAccount.id).map { pagingData ->
+            pagingData
+                .map { conversation ->
+                    val accountFilterDecision = if (conversation.isConversationStarter) {
+                        conversation.viewData?.accountFilterDecision
+                            ?: filterConversationByAccount(pachliAccount, conversation)
+                    } else {
+                        null
+                    }
+                    Pair(conversation, accountFilterDecision)
+                }
+                .filter { it.second !is AccountFilterDecision.Hide }
+                .map { (conversation, accountFilterDecision) ->
+                    ConversationViewData.make(
+                        pachliAccount,
+                        conversation,
+                        defaultIsExpanded = pachliAccount.entity.alwaysOpenSpoiler,
+                        defaultIsShowingContent = (pachliAccount.entity.alwaysShowSensitiveMedia || !conversation.lastStatus.status.sensitive),
+                        contentFilterAction = FilterAction.NONE,
+                        accountFilterDecision = accountFilterDecision,
+                    )
                 }
         }
-        .cachedIn(viewModelScope)
+    }.cachedIn(viewModelScope)
 
     val showFabWhileScrolling = sharedPreferencesRepository.changes
         .filter { it == null || it == PrefKeys.FAB_HIDE }

app/src/main/java/app/pachli/components/timeline/viewmodel/CachedTimelineViewModel.kt
@@ -67,15 +67,18 @@ class CachedTimelineViewModel @Inject constructor(
 ) {
     override val statuses = pachliAccountFlow.distinctUntilChangedBy { it.id }.flatMapLatest { pachliAccount ->
         repository.getStatusStream(pachliAccount.id, timeline).map { pagingData ->
-            pagingData.map {
-                StatusViewData.from(
-                    pachliAccountId = pachliAccount.id,
-                    it,
-                    isExpanded = pachliAccount.entity.alwaysOpenSpoiler,
-                    isShowingContent = pachliAccount.entity.alwaysShowSensitiveMedia,
-                    contentFilterAction = shouldFilterStatus(it.toStatus()),
-                )
-            }.filter { it.contentFilterAction != FilterAction.HIDE }
+            pagingData
+                .map { Pair(it, shouldFilterStatus(it)) }
+                .filter { it.second != FilterAction.HIDE }
+                .map { (timelineStatusWithAccount, contentFilterAction) ->
+                    StatusViewData.from(
+                        pachliAccountId = pachliAccount.id,
+                        timelineStatusWithAccount,
+                        isExpanded = pachliAccount.entity.alwaysOpenSpoiler,
+                        isShowingContent = pachliAccount.entity.alwaysShowSensitiveMedia,
+                        contentFilterAction = contentFilterAction,
+                    )
+                }
         }
     }.cachedIn(viewModelScope)
 

app/src/main/java/app/pachli/components/timeline/viewmodel/NetworkTimelineViewModel.kt
@@ -46,7 +46,7 @@ import com.github.michaelbull.result.onFailure
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.distinctUntilChangedBy
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
@@ -73,17 +73,20 @@ class NetworkTimelineViewModel @Inject constructor(
     statusDisplayOptionsRepository,
     sharedPreferencesRepository,
 ) {
-    override val statuses = pachliAccountId.distinctUntilChanged().flatMapLatest { pachliAccountId ->
-        repository.getStatusStream(pachliAccountId, timeline = timeline).map { pagingData ->
-            pagingData.map {
-                StatusViewData.from(
-                    pachliAccountId,
-                    it,
-                    isExpanded = statusDisplayOptions.value.openSpoiler,
-                    isShowingContent = statusDisplayOptions.value.showSensitiveMedia,
-                    contentFilterAction = shouldFilterStatus(it.toStatus()),
-                )
-            }.filter { it.contentFilterAction != FilterAction.HIDE }
+    override val statuses = pachliAccountFlow.distinctUntilChangedBy { it.id }.flatMapLatest { pachliAccount ->
+        repository.getStatusStream(pachliAccount.id, timeline = timeline).map { pagingData ->
+            pagingData
+                .map { Pair(it, shouldFilterStatus(it)) }
+                .filter { it.second != FilterAction.HIDE }
+                .map { (timelineStatusWithAccount, contentFilterAction) ->
+                    StatusViewData.from(
+                        pachliAccount.id,
+                        timelineStatusWithAccount,
+                        isExpanded = statusDisplayOptions.value.openSpoiler,
+                        isShowingContent = statusDisplayOptions.value.showSensitiveMedia,
+                        contentFilterAction = contentFilterAction,
+                    )
+                }
         }
     }.cachedIn(viewModelScope)
 

commit 4e8c07627b50a0acb152fd0bd80cf73643eacd4e

fix: Restore "filter boosts" functionality
Inadvertently broken in 9988932f187c8f26879675399745a5d9aa8f6ab6 which
used the wrong property in the test.

Reported by @lennart@mastodonsweden.se.

app/src/main/java/app/pachli/components/timeline/viewmodel/TimelineViewModel.kt
@@ -607,11 +607,12 @@ abstract class TimelineViewModel<T : Any, R : TimelineRepository<T>>(
         }
 
         val status = timelineStatus.status
+
         // Remove replies
-        if (status.inReplyToId != null && filterRemoveReplies) return FilterAction.HIDE
+        if (status.isReply && filterRemoveReplies) return FilterAction.HIDE
 
         // Remove boosts
-        if (status.reblogged && filterRemoveReblogs) return FilterAction.HIDE
+        if (status.isReblog && filterRemoveReblogs) return FilterAction.HIDE
 
         // Apply content filters.
         return contentFilterModel?.filterActionFor(status) ?: FilterAction.NONE

core/database/src/main/kotlin/app/pachli/core/database/model/StatusEntity.kt
@@ -21,6 +21,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.ForeignKey
+import androidx.room.Ignore
 import androidx.room.Index
 import androidx.room.TypeConverters
 import app.pachli.core.database.Converters
@@ -46,6 +47,12 @@ import java.util.Date
  * serialization).
  * "Reblog status", if present, is marked by [reblogServerId], and [reblogAccountId]
  * fields.
+ *
+ * @property reblogged True if [timelineUserId] reblogged this status.
+ * @property isReblog True if this status is a reblog of another status (see
+ * [reblogServerId] and [reblogAccountId])
+ * @property isReply True if this status is a reply to another status (see
+ * [inReplyToId] and [inReplyToAccountId])
  */
 @Entity(
     primaryKeys = ["serverId", "timelineUserId"],
@@ -106,6 +113,12 @@ data class StatusEntity(
     val language: String?,
     val filtered: List<FilterResult>?,
 ) {
+    @Ignore
+    val isReblog = reblogServerId != null
+
+    @Ignore
+    val isReply = inReplyToId != null
+
     companion object {
         fun from(status: Status, timelineUserId: Long) = StatusEntity(
             serverId = status.id,

core/model/src/main/kotlin/app/pachli/core/model/Status.kt
@@ -21,6 +21,9 @@ import app.pachli.core.common.extensions.getOrElse
 import com.squareup.moshi.JsonClass
 import java.util.Date
 
+/**
+ * @property reblogged True if the current user reblogged this status.
+ */
 data class Status(
     val id: String,
     // not present if it's reblog
@@ -53,7 +56,6 @@ data class Status(
     val language: String?,
     val filtered: List<FilterResult>?,
 ) {
-
     val actionableId: String
         get() = reblog?.id ?: id
 

core/network/src/main/kotlin/app/pachli/core/network/model/Status.kt
@@ -85,16 +85,6 @@ data class Status(
         DIRECT,
         ;
 
-        fun serverString(): String {
-            return when (this) {
-                PUBLIC -> "public"
-                UNLISTED -> "unlisted"
-                PRIVATE -> "private"
-                DIRECT -> "direct"
-                UNKNOWN -> "unknown"
-            }
-        }
-
         fun asModel() = when (this) {
             UNKNOWN -> app.pachli.core.model.Status.Visibility.UNKNOWN
             PUBLIC -> app.pachli.core.model.Status.Visibility.PUBLIC