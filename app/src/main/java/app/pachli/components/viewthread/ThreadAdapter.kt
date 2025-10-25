30e87dd4f62df9af7a8319bda728052172bb406a
Nik Clayton <nik@ngo.org.uk>
2025-10-25 15:02:59 +0200

    change: Remove unused `openUrl` parameter from ViewThreadAdapter

--- app/src/main/java/app/pachli/components/viewthread/ThreadAdapter.kt
+++ app/src/main/java/app/pachli/components/viewthread/ThreadAdapter.kt
@@ -24,7 +24,6 @@ import app.pachli.adapter.StatusBaseViewHolder
 import app.pachli.adapter.StatusDetailedViewHolder
 import app.pachli.adapter.StatusViewDataDiffCallback
 import app.pachli.adapter.StatusViewHolder
-import app.pachli.core.activity.OpenUrlUseCase
 import app.pachli.core.data.model.StatusDisplayOptions
 import app.pachli.core.data.model.StatusViewData
 import app.pachli.core.model.FilterAction
@@ -40,7 +39,6 @@ class ThreadAdapter(
     private val statusDisplayOptions: StatusDisplayOptions,
     private val statusActionListener: StatusActionListener<StatusViewData>,
     private val setStatusContent: SetStatusContent,
-    private val openUrl: OpenUrlUseCase,
 ) : ListAdapter<StatusViewData, StatusBaseViewHolder<StatusViewData>>(StatusViewDataDiffCallback) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StatusBaseViewHolder<StatusViewData> {
