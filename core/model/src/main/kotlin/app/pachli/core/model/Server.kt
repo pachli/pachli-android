080fa66dc63a9a445f8ab27e71c4973fbd99d660
cpf <git@cpf.sh>
2026-07-22 00:52:26 +0100

    Add Iceshrimp compatibility matrix

--- core/model/src/main/kotlin/app/pachli/core/model/Server.kt
+++ core/model/src/main/kotlin/app/pachli/core/model/Server.kt
@@ -430,6 +430,36 @@ data class Server(
                     c[ORG_JOINMASTODON_STATUSES_SCHEDULED] = "1.0.0".toVersion()
                 }
 
+                ICESHRIMP -> {
+                    c[ORG_JOINMASTODON_FILTERS_CLIENT] = "1.1.0".toVersion()
+                    c[ORG_JOINMASTODON_FILTERS_SERVER] = "1.0.0".toVersion()
+                    
+                    // https://iceshrimp.dev/iceshrimp/Iceshrimp.NET/src/branch/dev/Iceshrimp.Parsing/SearchQueryParser.cs
+                    c[ORG_JOINMASTODON_SEARCH_QUERY_FROM] = "1.1.0".toVersion()
+                    // no language: filter in iceshrimp
+                    c[ORG_JOINMASTODON_SEARCH_QUERY_HAS_MEDIA] = "1.0.0".toVersion()
+                    c[ORG_JOINMASTODON_SEARCH_QUERY_HAS_IMAGE] = "1.0.0".toVersion()
+                    c[ORG_JOINMASTODON_SEARCH_QUERY_HAS_VIDEO] = "1.0.0".toVersion()
+                    c[ORG_JOINMASTODON_SEARCH_QUERY_HAS_AUDIO] = "1.0.0".toVersion()
+                    c[ORG_JOINMASTODON_SEARCH_QUERY_HAS_POLL] = "1.0.0".toVersion()
+                    // no has:quote,link,embed (https://iceshrimp.dev/iceshrimp/Iceshrimp.NET/src/commit/0e18e2ee4db5a1197b9003f9f4e240081beb7c94/Iceshrimp.Parsing/SearchQueryFilters.cs#L107-L117)
+                    // iceshrimp can do is:reply and is:sensitive but uses different vocab for them
+                    // in:library is not supported, only the specific elements
+                    // likewise, in:public is referred to as visibility:public
+                    // (https://iceshrimp.dev/iceshrimp/Iceshrimp.NET/src/commit/0e18e2ee4db5a1197b9003f9f4e240081beb7c94/Iceshrimp.Parsing/SearchQueryFilters.cs#L65-L89)
+                    
+                    // partial date support is included (no during/in); however, I think this is what pachli uses rn anyway?
+                    c[ORG_JOINMASTODON_SEARCH_QUERY_BY_DATE] = "1.0.0".toVersion()
+                    
+                    // https://iceshrimp.dev/iceshrimp/Iceshrimp.NET/src/commit/5202831d4c3cea2eba121f8bfd493b6080d3a82c/Iceshrimp.Backend/Controllers/Mastodon/StatusController.cs#L77-L100
+                    c[ORG_JOINMASTODON_STATUSES_GET] = "1.0.0".toVersion()
+                    
+                    c[ORG_JOINMASTODON_STATUSES_SCHEDULED] = "1.0.0".toVersion()
+
+                    // quoting but no quote policy yet: for the time being, advertises all posts as quotable (https://iceshrimp.dev/iceshrimp/Iceshrimp.NET/pulls/212)
+                    c[ORG_JOINMASTODON_STATUSES_QUOTE] = "1.0.0".toVersion()
+                }
+
                 // Everything else. Assume:
                 //
                 // - server side filtering
@@ -437,7 +467,7 @@ data class Server(
                 // - no translation
                 //
                 // This may be an incorrect assumption.
-                FEDIBIRD, ICESHRIMP, PIXELFED, UNKNOWN -> {
+                FEDIBIRD, PIXELFED, UNKNOWN -> {
                     c[ORG_JOINMASTODON_FILTERS_SERVER] = "1.0.0".toVersion()
                     c[ORG_JOINMASTODON_STATUSES_SCHEDULED] = "1.0.0".toVersion()
                 }
